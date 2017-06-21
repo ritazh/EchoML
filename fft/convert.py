dd3f264c146668f1b0102a5c883af9dbb3b497d6
Rita Zhang <rita.z.zhang@gmail.com>
2017-06-21 16:14:45 -0700

    Update converting audio to image

--- fft/convert.py
+++ fft/convert.py
@@ -7,10 +7,17 @@ Fs = int(1.0/dt)
 
 current_dir = os.path.dirname(__file__)
 
-
-path = os.path.join(current_dir + "/SM01_20091211_011000.flac")
-print (path)
-soundfile = Sndfile(path, "r")
-signal = soundfile.read_frames(soundfile.nframes)
-plt.specgram(signal, NFFT=256, Fs=Fs)
-plt.savefig('fft/foo.png') 
+for (dirpath, dirnames, filenames) in os.walk(current_dir):
+    for filename in filenames:
+		path = os.path.join(current_dir + "/" + filename)
+		print (path)
+		fname, extension = os.path.splitext(path)
+		print (extension)
+		if extension == '.flac':
+			soundfile = Sndfile(path, "r")
+			signal = soundfile.read_frames(soundfile.nframes)
+			plt.specgram(signal, NFFT=256, Fs=Fs)
+			
+			outputfile = path.replace(extension, '.png')
+			print (outputfile)
+			plt.savefig(outputfile) 
