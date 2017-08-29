e96559f5acf544a3fd9c55e5cadcbf61a9eb0ec3
Evan Louie <evan.louie@microsoft.com>
2017-08-29 16:41:07 -0700

    Add cluster/multi-thread support

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 const config = require('config');
 const createServer = require('./server');
 
 const hostname = config.get('hostname');
 const port = config.get('port');
-createServer(hostname, port);
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  createServer(hostname, port);
+  console.log(`Worker ${process.pid} started`);
+}
