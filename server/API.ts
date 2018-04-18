eb7b4d9a49b2af7527bb01add2df28072cc6949e
Evan Louie <evan.louie@microsoft.com>
2018-04-18 16:01:41 -0700

    Fix Filename Casing Error
    - Fixed a bug where OSX filename case insensitivity caused made builds
    on linux systems fail

---
+++ server/API.ts
@@ -0,0 +1,24 @@
+import * as Koa from "koa";
+import * as _ from "koa-route";
+import { AccountsController } from "./controllers/AccountsController";
+import { BlobsController } from "./controllers/BlobsController";
+import { ContainersController } from "./controllers/ContainersController";
+import { LabelsController } from "./controllers/LabelsController";
+
+export class API {
+  public static routes: Koa.Middleware[] = [
+    _.get("/api/accounts", AccountsController.index),
+    _.get("/api/blobs", BlobsController.index),
+    _.get(
+      "/api/blobs/download/:storage_account/:container_name/:filename(.+)",
+      BlobsController.download,
+    ),
+    _.get("/api/containers", ContainersController.index),
+    _.get("/api/containers/:name", ContainersController.show),
+    _.get("/api/containers/:name/blobs", ContainersController.blobs),
+    _.get("/api/containers/:name/labels", ContainersController.labels),
+    _.get("/api/labels", LabelsController.index),
+    _.get("/api/labels/:storage_account/:container_name/:filename(.+)", LabelsController.show),
+    _.post("/api/labels", LabelsController.store),
+  ];
+}
